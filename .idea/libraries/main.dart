664cbf55b29714b42b899b39f427260422a2dcf6
pervez-hasan <phparvez71@gmail.com>
2023-01-08 18:45:08 +0600

    interface

---
+++ .idea/libraries/main.dart
@@ -0,0 +1,20 @@
+void main(){
+  print("Interface class");
+}
+
+
+class nomal implements interfaceclass{
+  void math(){
+
+    print("Normal class");
+  }
+
+}
+
+class interface{ //interface class create
+
+  void name(){} //interface method create
+  void age(){}
+  void son(){}
+
+}
