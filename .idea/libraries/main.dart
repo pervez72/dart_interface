502911620a1c2b2e209b73ce557a61c106be92ef
pervez-hasan <phparvez71@gmail.com>
2023-01-08 20:07:31 +0600

    interface basic

--- .idea/libraries/main.dart
+++ .idea/libraries/main.dart
@@ -1,17 +1,37 @@
 void main(){
+  /*
+  interface: this is a class.this class into method empty thakbe.implement
+  korar por implemnet korte hobe
+   */
   print("Interface class");
+
+  var obj=new nomal();
+  obj.math();
+  obj.name();
+  obj.age();
+  obj.son();
 }
 
 
 class nomal implements interfaceclass{
   void math(){
-
     print("Normal class");
   }
-
+  @override
+  void name(){
+    print("hasan");
+  }
+  @override
+  void age(){
+    print("21");
+  }
+  @override
+  void son(){
+    print("upcomeing");
+  }
 }
 
-class interface{ //interface class create
+class interfaceclass{ //interface class create
 
   void name(){} //interface method create
   void age(){}
